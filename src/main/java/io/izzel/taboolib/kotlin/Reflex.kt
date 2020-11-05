e12a3402c7468662da28bb0eb8a49667a69e636a
sky <sky@asgard.me>
2020-11-05 14:29:59 +0800

    Fix Reflex

--- src/main/java/io/izzel/taboolib/kotlin/Reflex.kt
+++ src/main/java/io/izzel/taboolib/kotlin/Reflex.kt
@@ -20,6 +20,39 @@ class Reflex(val from: Class<*>) {
         return this
     }
 
+    fun <T> read(name: String): T? {
+        val deep = name.indexOf("/")
+        if (deep == -1) {
+            return get(name)
+        }
+        var find: T? = null
+        var ref = of(get(name.substring(0, deep))!!)
+        name.substring(deep).split("/").filter { it.isNotEmpty() }.forEach { point ->
+            find = ref.get(point)
+            if (find != null) {
+                ref = of(find!!)
+            }
+        }
+        return find
+    }
+
+    fun write(name: String, value: Any?) {
+        val deep = name.indexOf("/")
+        if (deep == -1) {
+            return set(name, value)
+        }
+        val node0 = name.substring(0, deep)
+        val node1 = name.substring(name.lastIndexOf("/") + 1, name.length)
+        val space = name.substring(deep).split("/").filter { it.isNotEmpty() }
+        var ref = of(get(node0)!!)
+        space.forEachIndexed { index, point ->
+            if (index + 1 < space.size) {
+                ref = of(ref.get(point)!!)
+            }
+        }
+        ref.set(node1, value)
+    }
+
     fun <T> get(type: Class<T>, index: Int = 0): T? {
         val field = cachedField.computeIfAbsent(from.name) {
             Ref.getDeclaredFields(from).map {
@@ -78,6 +111,8 @@ class Reflex(val from: Class<*>) {
 
         private val cachedField = Maps.newConcurrentMap<String, Map<String, Field>>()
         private val cachedMethod = Maps.newConcurrentMap<String, Map<String, Method>>()
+
+        fun of(instance: Any): Reflex = Reflex(instance.javaClass).instance(instance)
 
         fun from(clazz: Class<*>): Reflex = Reflex(clazz)
 
